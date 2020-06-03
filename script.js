5a40912a211e9503decc9e1852e07d4f76b64768
RNgugi <reubeenkihiu@gmail.com>
2020-06-03 04:55:27 +0300

    seat selector working

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
+//update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+
+count.innerText = selectedSeatsCount;
+total.innerText = selectedSeatsCount * ticketPrice;
+}
+
 container.addEventListener('click', (e) => {
-    console.log(e.target);
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
 });
